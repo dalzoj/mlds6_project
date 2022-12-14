337c186375fd31f8ce2b05a9d3bc76b47ad43b3d
Diego Lizarazo <imdlizarazo@gmail.com>
2022-12-14 18:47:52 -0500

    Delete: Carpetas innecesarias

--- mlds6_project/modeling/save_model.py
+++ mlds6_project/modeling/save_model.py
@@ -1,7 +1,5 @@
 import joblib
-import shutil
-from os import remove
 
 def save_model(model, name='model'):
-    with open('../mlds6_project/models/'+name+'.pkl', 'wb') as f:
+    with open('mlds6_project/models/'+name+'.pkl', 'wb') as f:
         joblib.dump(model, f)
